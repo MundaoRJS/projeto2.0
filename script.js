a4f06ac194a84322a4ccc1a9534f8a7fcec7852c
RAIMUNDO JUNIO SANTOS <raimundojuniosantos@outlook.com.br>
2023-01-18 20:22:31 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,18 @@
+let time = 5000,
+    currentImageIndex=0,
+    images = document
+                .querySelectorAll("#cartaz-prinipal img")
+    max = images.length;
+function nextImage(){
+    
+    images[currentImageIndex]
+        .classList.remove("cartaz")
+
+    currentImageIndex++
+    
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("cartaz")
+}
